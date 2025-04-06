cbc2a19b88bfefdac071b02ab89891c97b00361e
JaskaranSingh2 <jasonkaura@gmail.com>
2025-04-06 16:18:56 -0600

    com

--- assignments/a5/q3/fun.cc
+++ assignments/a5/q3/fun.cc
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "I enjoyed learning about memory management and how it gives more control than garbage-collected languages." << std::endl;
+    std::cout << "I enjoyed learning about memory management and why and how it gives more control than garbage collected languages" << std::endl;
     std::cout << "The ability to create efficient data structures in C++ is pretty interesting" << std::endl;
-    std::cout << "I enjoyed learning pointers and references, which helped me become a better programmer overall." << std::endl;
+    std::cout << "I enjoyed learning pointers and references, which helped me become a better programmer overall" << std::endl;
     
     return 0;
 }
